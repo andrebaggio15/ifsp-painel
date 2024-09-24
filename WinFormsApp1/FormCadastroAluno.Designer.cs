a963d0e4212422e27ffffa0c1d36d044edd24aed
andrebaggio15 <andreingrato15@gmail.com>
2024-09-24 10:39:41 -0300

    Add files via upload

---
+++ WinFormsApp1/FormCadastroAluno.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WinFormsApp1
+{
+    partial class FormCadastroAluno
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormCadastroAluno));
+            materialTabControl1 = new ReaLTaiizor.Controls.MaterialTabControl();
+            FomrCadastroAluno = new TabPage();
+            tabPage2 = new TabPage();
+            imageList1 = new ImageList(components);
+            materialMaskedTextBox1 = new ReaLTaiizor.Controls.MaterialMaskedTextBox();
+            materialTabControl1.SuspendLayout();
+            FomrCadastroAluno.SuspendLayout();
+            SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            materialTabControl1.Controls.Add(FomrCadastroAluno);
+            materialTabControl1.Controls.Add(tabPage2);
+            materialTabControl1.Depth = 0;
+            materialTabControl1.Dock = DockStyle.Fill;
+            materialTabControl1.ImageList = imageList1;
+            materialTabControl1.Location = new Point(3, 64);
+            materialTabControl1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.HOVER;
+            materialTabControl1.Multiline = true;
+            materialTabControl1.Name = "materialTabControl1";
+            materialTabControl1.SelectedIndex = 0;
+            materialTabControl1.Size = new Size(604, 383);
+            materialTabControl1.TabIndex = 0;
+            materialTabControl1.SelectedIndexChanged += materialTabControl1_SelectedIndexChanged;
+            // 
+            // FomrCadastroAluno
+            // 
+            FomrCadastroAluno.Controls.Add(materialMaskedTextBox1);
+            FomrCadastroAluno.ImageKey = "form.png";
+            FomrCadastroAluno.Location = new Point(4, 31);
+            FomrCadastroAluno.Name = "FomrCadastroAluno";
+            FomrCadastroAluno.Padding = new Padding(3);
+            FomrCadastroAluno.Size = new Size(596, 348);
+            FomrCadastroAluno.TabIndex = 0;
+            FomrCadastroAluno.Text = "Cadastro";
+            FomrCadastroAluno.UseVisualStyleBackColor = true;
+            FomrCadastroAluno.Click += tabPage1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.ImageKey = "search.png";
+            tabPage2.Location = new Point(4, 31);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(596, 348);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Consulta";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "search.png");
+            imageList1.Images.SetKeyName(1, "form.png");
+            // 
+            // materialMaskedTextBox1
+            // 
+            materialMaskedTextBox1.AllowPromptAsInput = true;
+            materialMaskedTextBox1.AnimateReadOnly = false;
+            materialMaskedTextBox1.AsciiOnly = false;
+            materialMaskedTextBox1.BackgroundImageLayout = ImageLayout.None;
+            materialMaskedTextBox1.BeepOnError = false;
+            materialMaskedTextBox1.CutCopyMaskFormat = MaskFormat.IncludeLiterals;
+            materialMaskedTextBox1.Depth = 0;
+            materialMaskedTextBox1.Font = new Font("Roboto", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialMaskedTextBox1.HidePromptOnLeave = false;
+            materialMaskedTextBox1.HideSelection = true;
+            materialMaskedTextBox1.InsertKeyMode = InsertKeyMode.Default;
+            materialMaskedTextBox1.LeadingIcon = null;
+            materialMaskedTextBox1.Location = new Point(-41, -19);
+            materialMaskedTextBox1.Mask = "";
+            materialMaskedTextBox1.MaxLength = 32767;
+            materialMaskedTextBox1.MouseState = ReaLTaiizor.Helper.MaterialDrawHelper.MaterialMouseState.OUT;
+            materialMaskedTextBox1.Name = "materialMaskedTextBox1";
+            materialMaskedTextBox1.PasswordChar = '\0';
+            materialMaskedTextBox1.PrefixSuffixText = null;
+            materialMaskedTextBox1.PromptChar = '_';
+            materialMaskedTextBox1.ReadOnly = false;
+            materialMaskedTextBox1.RejectInputOnFirstFailure = false;
+            materialMaskedTextBox1.ResetOnPrompt = true;
+            materialMaskedTextBox1.ResetOnSpace = true;
+            materialMaskedTextBox1.RightToLeft = RightToLeft.No;
+            materialMaskedTextBox1.SelectedText = "";
+            materialMaskedTextBox1.SelectionLength = 0;
+            materialMaskedTextBox1.SelectionStart = 0;
+            materialMaskedTextBox1.ShortcutsEnabled = true;
+            materialMaskedTextBox1.Size = new Size(312, 48);
+            materialMaskedTextBox1.SkipLiterals = true;
+            materialMaskedTextBox1.TabIndex = 0;
+            materialMaskedTextBox1.TabStop = false;
+            materialMaskedTextBox1.Text = "materialMaskedTextBox1";
+            materialMaskedTextBox1.TextAlign = HorizontalAlignment.Left;
+            materialMaskedTextBox1.TextMaskFormat = MaskFormat.IncludeLiterals;
+            materialMaskedTextBox1.TrailingIcon = null;
+            materialMaskedTextBox1.UseSystemPasswordChar = false;
+            materialMaskedTextBox1.ValidatingType = null;
+            materialMaskedTextBox1.Click += materialMaskedTextBox1_Click;
+            // 
+            // FormCadastroAluno
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 450);
+            Controls.Add(materialTabControl1);
+            DrawerTabControl = materialTabControl1;
+            Name = "FormCadastroAluno";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Cadastro de Alunos";
+            Load += FormCadastroAluno_Load;
+            materialTabControl1.ResumeLayout(false);
+            FomrCadastroAluno.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Controls.MaterialTabControl materialTabControl1;
+        private TabPage FomrCadastroAluno;
+        private TabPage tabPage2;
+        private ImageList imageList1;
+        private ReaLTaiizor.Controls.MaterialMaskedTextBox materialMaskedTextBox1;
+    }
+}
---
+++ WinFormsApp1/FormCadastroAluno.cs
@@ -0,0 +1,41 @@
+﻿using ReaLTaiizor.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class FormCadastroAluno : MaterialForm
+    {
+        public FormCadastroAluno()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialTabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormCadastroAluno_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void materialMaskedTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
